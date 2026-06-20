809b81e25f0a22eaa9d093facd54ac864f437ea4
AR F <deivyriquett13@gmail.com>
2026-06-19 19:58:49 -0500

    Creacion proyecto Spring Boot

---
+++ backend-java/backend-springboot/src/main/java/com/sena/sistema_credenciales/SistemaCredencialesApplication.java
@@ -0,0 +1,13 @@
+package com.sena.sistema_credenciales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCredencialesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCredencialesApplication.class, args);
+	}
+
+}
---
+++ backend-java/backend-springboot/src/test/java/com/sena/sistema_credenciales/SistemaCredencialesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.sistema_credenciales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCredencialesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
